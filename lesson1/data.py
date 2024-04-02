ad1b90cae5f4f7c7e017d4760541d2b75cbfe52a
JuliaKuznetsovaQA <kuznetsovajulia@gmail.com>
2024-04-03 01:29:00 +0300

    add files for generation user's firstname, lastname and postcode

--- lesson1/data.py
+++ lesson1/data.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 # AUTH
 
 login = 'standard_user'
@@ -8,3 +10,12 @@ password = 'secret_sauce'
 main_page = 'https://www.saucedemo.com/'
 catalogue = 'https://www.saucedemo.com/inventory.html'
 
+
+@dataclass
+class User:
+    first_name: str = None,
+    last_name: str = None,
+    full_name: str = None,
+    address: str = None,
+    phone_number: str = None,
+    postcode: str = None
--- lesson1/test_purchase.py
+++ lesson1/test_purchase.py
@@ -1,18 +1,23 @@
 from selenium.webdriver.common.by import By
 from locators import *
 from data import *
+from generator import generated_user
 
 
 """Оформление заказа"""
+
+
 def test_purchase(auth):
     """Оформление заказа используя корректные данные"""
+    """генерация данных пользователя"""
+    user = next(generated_user())
     auth.find_element(By.XPATH, t_shirt_red_image).click()
     auth.find_element(By.ID, 'add-to-cart').click()
     auth.find_element(By.ID, 'shopping_cart_container').click()
     auth.find_element(By.ID, 'checkout').click()
-    auth.find_element(By.ID, 'first-name').send_keys('James')
-    auth.find_element(By.ID, 'last-name').send_keys('Smith')
-    auth.find_element(By.ID, 'postal-code').send_keys('901234')
+    auth.find_element(By.ID, 'first-name').send_keys(user.first_name)
+    auth.find_element(By.ID, 'last-name').send_keys(user.last_name)
+    auth.find_element(By.ID, 'postal-code').send_keys(user.postcode)
     auth.find_element(By.ID, 'continue').click()
     assert auth.find_element(By.ID, 'item_3_title_link').is_displayed()
     assert auth.find_element(By.XPATH, '//*[@id="checkout_summary_container"]/div/div[1]/div[3]/div[1]').text == '1'
